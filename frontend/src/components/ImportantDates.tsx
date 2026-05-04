8d31a086ce865a81c569f863e152ec97cf4c2d1a
Nirali Gupta <niralig.8269@gmail.com>
2026-05-04 15:51:24 +0530

    updated

--- frontend/src/components/ImportantDates.tsx
+++ frontend/src/components/ImportantDates.tsx
@@ -5,6 +5,7 @@ const importantDates = [
     { date: "19 March, 2026", event: "Registration Opens" },
     { date: <span><s>28 March, 2026</s> 30 April, 2026</span>, event: "Early Bird Registration Closes" },
     { date: <span><s>15 April, 2026</s> 15 May, 2026</span>, event: "Late Registration Closes" },
+    { date: "16 May, 2026", event: "Online Pre-School Session" },
     { date: "1 June, 2026", event: "School Starts" },
 ];
 
